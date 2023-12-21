54899e620f1802b15cc9f40aa128e94b445cacf9
OzDelgado <oscaridcortes@gmail.com>
2023-12-20 18:29:48 -0600

    PLANS

--- src/controllers/plan.controller.js
+++ src/controllers/plan.controller.js
@@ -36,6 +36,67 @@ async function updatePlan (req = request, res = response){
     }
 }
 
+async function createPlan (req = request, res = response){
+    const {name} = req.body;
+    if(!name){
+        return res.status(400).json({
+            ok: false,
+            status: 400,
+            msg: "El nombre es obligatorio"
+        })
+    }
+    try {
+        const [plan] = await promisePool.query("SELECT id FROM plan WHERE name = ?", [name]);
+        if(plan.length){
+            return res.status(400).json({
+                ok: false,
+                status: 400,
+                msg: "El plan ya existe"
+            })
+        }
+        await promisePool.query("INSERT INTO plan SET ?", [{
+            id_plan: generateID(),
+            name: name.trim(),
+        }])
+        return res.status(200).json({
+            ok:true,
+            status:200,
+            msg: "Plan creado"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            info: {
+                ok: false, 
+                status: 500,
+                msg: "Error inesperado intentalo mas tarde",
+                error
+            }
+        })
+    }
+}
+
+async function getPlans (req = request, res = response){
+    try {
+        const [plans] = await promisePool.query("SELECT * FROM plan");
+        return res.status(200).json({
+            ok:true,
+            status:200,
+            plans
+        })
+    } catch (error) {
+        return res.status(500).json({
+            info: {
+                ok: false, 
+                status: 500,
+                msg: "Error inesperado intentalo mas tarde",
+                error
+            }
+        })
+    }
+}
+
 module.exports = {
-    updatePlan
+    updatePlan,
+    createPlan,
+    getPlans
 }
--- src/routes/plan.routes.js
+++ src/routes/plan.routes.js
@@ -4,9 +4,13 @@ const routes = express.Router();
 const { isAuthenticated } = require("../middlewares/authentication.middleware.js");
 
 const {
-    updatePlan
+    updatePlan,
+    createPlan,
+    getPlans
 } = require('../controllers/plan.controller.js')
 
 routes.put('/plan', updatePlan)
+routes.post('/createplan', createPlan)
+routes.get('/getplans', getPlans)
 
 module.exports = routes;
